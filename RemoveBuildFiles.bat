cc1680c750725da52bf8dda7ec89faa85234d347
KhArtNJava <khartnjava@gmail.com>
2015-07-09 14:32:21 +0300

    Updated bat for cleaning...

--- RemoveBuildFiles.bat
+++ RemoveBuildFiles.bat
@@ -1,4 +1,7 @@
 rmdir /S /Q Binaries
-rmdir /S /Q Intermediate\Build
+rmdir /S /Q Intermediate
+rmdir /S /Q Saved
+rmdir /S /Q Plugins\SQLite3UE4Plugin\Binaries
+rmdir /S /Q Plugins\SQLite3UE4Plugin\Intermediate
 
 SQLite.uproject

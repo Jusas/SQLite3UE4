4e09ce8c8ad08199632067f86025b28cf38660d5
Arthur Khusnutdiov <forpdfsending@gmail.com>
2016-04-15 10:44:38 +0300

    Fixes for UE 4.11 Release and UE 4.12 (from Main branch)

--- Plugins/SQLite3UE4Plugin/Source/SQLite3UE4Plugin/SQLite3UE4Plugin.Build.cs
+++ Plugins/SQLite3UE4Plugin/Source/SQLite3UE4Plugin/SQLite3UE4Plugin.Build.cs
@@ -5,7 +5,18 @@ public class SQLite3UE4Plugin : ModuleRules
 {
     public SQLite3UE4Plugin(TargetInfo Target)
     {
-        string ModulePath = Path.GetDirectoryName( RulesCompiler.GetModuleFilename( this.GetType().Name ) );
+		//File.WriteAllText("c:/temp/qqq.txt", this.GetType().Name);
+		//string ModulePath = Path.GetDirectoryName( RulesAssembly.GetModuleFilename( this.GetType().Name ) );
+		
+		RulesAssembly r;
+		FileReference CheckProjectFile;
+		UProjectInfo.TryGetProjectForTarget("MyGame", out CheckProjectFile);
+		
+		r = RulesCompiler.CreateProjectRulesAssembly(CheckProjectFile);
+		FileReference f = r.GetModuleFileName( this.GetType().Name );
+		//File.WriteAllText("c:/temp/qqq2.txt", f.CanonicalName );
+		
+        string ModulePath = Path.GetDirectoryName( f.CanonicalName );
         string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
         string ThirdPartyPath = Path.GetFullPath( Path.Combine( ModulePath, "../../ThirdParty/" ) );
         string LibrariesPath = Path.Combine(ThirdPartyPath, "SQLite3", "Lib");
